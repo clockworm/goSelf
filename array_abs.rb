cf376a5476dbf491022f1825a4f1986a66c17a17
mick <345684180@qq.com>
2015-05-01 23:29:54 +0800

    最小绝对值
    2个集合中的元素相减最小绝对值

--- array_abs.rb
+++ array_abs.rb
@@ -1,8 +1,7 @@
-=begin
-	
+=begin	
 一个问题，有两个集合A和B，a是A中的元素，b是B中的元素，
 求解a与b之差绝对值 的最小值
-	
+不知道还有其他写法不,多多指教
 =end
 
 a=[]
@@ -11,17 +10,18 @@ i=0
 c=0
 x=0
 
-while i<5
-		a[i]=rand(1)
-		b[i]=rand(1)
-		print "#{a[i]}   \n " 
-		print "   #{b[i]}   \n" 
+while i<28
+		a[i]=rand(999)
+		b[i]=rand(999)
+	#	print "#{a[i]}   \n " 
+	#	print "   #{b[i]}   \n" 
 	i+=1
 end
-min=(a[0]-b[0]).abs
-if min == 0 then
-	min = 1	
-end
+
+	min=(a[0]-b[0]).abs
+	if  min == 0 then
+		min = 1	
+	end
 
 while(x<a.size) do
 	y=0
@@ -33,8 +33,8 @@ while(x<a.size) do
 				  min = c.abs
 				end
 			end
-		y+=1
+	y+=1
 	end
-	x+=1 
+x+=1 
 end
 puts "最小绝对值为 #{min}"
