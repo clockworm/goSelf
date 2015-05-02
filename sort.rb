efc21f3254aa8387be41bfb8f0c743d03ee69292
mick <345684180@qq.com>
2015-05-02 22:34:10 +0800

    追加ruby自带排序
    字符串排序示例

--- sort.rb
+++ sort.rb
@@ -1,11 +1,15 @@
 a=[]
 b=[]
 i=0
-while i<22
+while i<10
 	a[i]=rand(99999)
 	i+=1
 end
 
+# p a.sort_by{|x| x} 顺序排序
+# p a.sort_by &:-@   逆向排序
+
+ 
 # 冒泡排序
 i=0
 while i<a.size
@@ -26,5 +30,29 @@ while 0 < a.size
 # 	a.delete(a.max)  #直接删元素 时间效率低  
  	a.delete_at(a.index a.max)   #直接删元素所对应的坐标 时间效率相对高点
 end
-    a = b
+#h = {'a'=>1,'b'=>2,'c'=>5,'d'=>4}
+#h.sort {|a,b| a[1]<=>b[1]}
+    a = b 
  puts "        #{a}" 
+
+#字符串排序
+
+str = ["192.160.175","192.160.90","192.168.2.175","192.168.2.90","192.168.1.180","192.160.90.1"]
+
+arr = str.sort do |a, b|
+    ip1 = a.split('.').map &:to_i
+    ip2 = b.split('.').map &:to_i
+    ret = 0
+    ip1.each_with_index do |ip, i|
+      if ip2[i].nil?
+        ret = 1
+        break
+      end
+      if ip != ip2[i]
+        ret = ip <=> ip2[i]
+        break
+      end
+    end
+    ret = ret == 0 && ip1.size > ip2.size ? 1 : ret
+end
+p arr
