2892b8bc5430a71acdeedce8abbe644735f2e7b0
mick <345684180@qq.com>
2015-05-02 23:07:07 +0800

    追加自带排序

--- sort.rb
+++ sort.rb
@@ -1,16 +1,18 @@
 a=[]
 b=[]
 i=0
-while i<10
-	a[i]=rand(99999)
+while i<12   #10000
+	a[i]=rand(999999)  #999999
 	i+=1
 end
 
-# p a.sort_by{|x| x} 顺序排序
-# p a.sort_by &:-@   逆向排序
+# a= a.sort   #默认顺序排序 0.004 
 
- 
-# 冒泡排序
+# a = a.sort_by{|x| x} #顺序排序  0.018
+
+# a= a.sort_by &:-@   #逆向排序  0.018
+
+# 冒泡排序   12.55
 i=0
 while i<a.size
 	j=i+1
@@ -22,20 +24,19 @@ while i<a.size
 	end
 	i+=1
 end
- puts "        #{a} " 
+p a
 
- # 选择排序
+# 选择排序   16.8(删坐标)  23.2(直接删元素)
 while 0 < a.size
  	b << a.max
-# 	a.delete(a.max)  #直接删元素 时间效率低  
+#	a.delete(a.max)  #直接删元素 时间效率低  
  	a.delete_at(a.index a.max)   #直接删元素所对应的坐标 时间效率相对高点
 end
-#h = {'a'=>1,'b'=>2,'c'=>5,'d'=>4}
-#h.sort {|a,b| a[1]<=>b[1]}
-    a = b 
- puts "        #{a}" 
+  	a = b 
+p a
 
-#字符串排序
+
+#字符串 排序
 
 str = ["192.160.175","192.160.90","192.168.2.175","192.168.2.90","192.168.1.180","192.160.90.1"]
 
